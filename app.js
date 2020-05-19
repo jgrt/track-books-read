5bf95542356c819d27f718e5868c116707da773f
jgrt <asati.jagrati93@gmail.com>
2020-05-19 23:00:55 +0530

    Added swagger with OpenAPI 3 specifications by using jsdoc module

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
 const express = require('express')
 const app = express()
-const bookRouter = require("./routes/books");
-app.use("/books", bookRouter);
 
-app.listen(3000, "0.0.0.0" , () => {
- console.log("server started");
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const bookRouter = require("./routes/books");
+app.use("/library", bookRouter);
+
+const swaggerDefinition = {
+    info: {
+            title: 'Books API',
+            description: "Book api doc"
+    }
+};
+const options = {
+  swaggerDefinition,
+  apis: ['./routes/books.js'],
+};
+
+const swaggerDocs = swaggerJsDoc(options);
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+
+
+
+const port = 3000;
+const host = '0.0.0.0';
+
+app.listen(port, host , () => {
+ console.log(`server listening on port ${port}`);
 })
--- routes/books.js
+++ routes/books.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
-router.get('/', function (req, res) {
-  res.send('Get all Books')
+/**
+* @swagger
+* /library/books:
+*   get:
+*       description: use to get all books
+*       responses:
+*           '200':
+*               description: A successful response
+*/
+router.get('/books', function (req, res) {
+  res.send('Get Books');
 });
 
 module.exports = router;
